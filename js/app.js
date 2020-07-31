89aa40f5e0c71a763430ea3626c3c78a351a4fd7
Chase McFaddin <comcfadd@gmail.com>
2020-07-30 21:04:15 -0700

    finished lab4

--- js/app.js
+++ js/app.js
@@ -11,109 +11,117 @@ function question1() {
   var response1 = prompt('1. Does my first name have two "t\'s"? Yes or no?');
   var answer1 = response1.toLowerCase();
   if (answer1 === 'yes' || answer1 === 'y') {
-    alert ('Yes, my name does have two t\'s!');
+    alert('Yes, my name does have two t\'s!');
     a++;
   } else if (answer1 === 'no' || answer1 === 'n') {
-    alert ('Actually my name does have two t\'s');
+    alert('Actually my name does have two t\'s');
   }
-}
+} question1();
 
 function question2() {
   var response2 = prompt('2. Have I ever left the U.S.?');
   var answer2 = response2.toLowerCase();
   if (answer2 === 'yes' || answer2 === 'y') {
-    alert ('Correct! I\'ve been to about 10 countries!');
+    alert('Correct! I\'ve been to about 10 countries!');
     a++;
   } else if (answer2 === 'no' || answer2 === 'n') {
-    alert ('Incorrect! I\'ve been to about 10 countries');
+    alert('Incorrect! I\'ve been to about 10 countries');
   }
-}
+} question2();
 
 function question3() {
   var response3 = prompt('3. Did I join the Army?');
   var answer3 = response3.toLowerCase();
   if (answer3 === 'yes' || answer3 === 'y') {
-    alert ('Incorrect! I served 4 years active duty in the Navy!');
+    alert('Incorrect! I served 4 years active duty in the Navy!');
   } else if (answer3 === 'no' || answer3 === 'n') {
-    alert ('Correct! I served 4 years active duty in the Navy!');
+    alert('Correct! I served 4 years active duty in the Navy!');
     a++;
   }
-}
+} question3();
 
 function question4() {
   var response4 = prompt('4. Do I have any pets?');
   var answer4 = response4.toLowerCase();
   if (answer4 === 'yes' || answer4 === 'y') {
-    alert ('Correct! I have a beautiful puppy named Macy!');
+    alert('Correct! I have a beautiful puppy named Macy!');
     a++;
   } else if (answer4 === 'no' || answer4 === 'n') {
-    alert ('Incorrect! I have a beautiful puppy named Macy!');
+    alert('Incorrect! I have a beautiful puppy named Macy!');
   }
-}
+} question4();
 
 function question5() {
   var response5 = prompt('5. Am I interested in robotics?');
   var answer5 = response5.toLowerCase();
   if (answer5 === 'yes' || answer5 === 'y') {
-    alert ('Correct! It\'s my passion!');
+    alert('Correct! It\'s my passion!');
     a++;
   } else if (answer5 === 'no' || answer5 === 'n') {
-    alert ('Incorrect! It\'s my passion!');
+    alert('Incorrect! It\'s my passion!');
+  }
+} question5();
+
+
+
+function question6() {
+  var response6 = prompt('How many years did I spend at my permanent duty station?');
+  var i = 0;
+  while (i <= 4) {
+    if (response6 == 3) {
+      alert('Correct! I spent 3 years at my permanent duty station and about 1 year between laerning to be the best sailor I could be and taking leave at the end of my contract to prepare myself for civilian life.');
+      a++;
+      break;
+    }
+    if (response6 < 3) {
+      response6 = prompt('Incorrect! Too low, try again.');
+      i++;
+    }
+    if (response6 > 3) {
+      response6 = prompt('Incorrect! Too high, try again.');
+      i++;
+    } else {
+      alert('Incorrect! I spent 3 years at my permanent duty station and about 1 year between laerning to be the best sailor I could be and taking leave at the end of my contract to prepare myself for civilian life.');
+      i++;
+    }
+    console.log(response6);
   }
 }
+question6();
 
-// var response6 = prompt('How many years did I spend at my permanent duty station?');
-// var i = 0;
-// while (i <= 3) {
-//   if (response6 === 3) { break; }
-//   while (response6 < 3) {
-//     i++;
-//     response6 = prompt('Incorrect! Too low, try again.');
-//     break;
-//   }
-//   while (response6 > 3) {
-//     i++;
-//     response6 = prompt('Incorrect! Too high, try again.');
-//     break;
-//   }
-//   if (i === 3) { break; }
-// }
-// if (response6 === 3) {
-//   alert('Correct! I spent 3 years at my permanent duty station and about 1 year between laerning to be the best sailor I could be and taking leave at the end of my contract to prepare myself for civilian life.');
-//   a++;
-// } else {
-//   alert('Incorrect! I spent 3 years at my permanent duty station and about 1 year between laerning to be the best sailor I could be and taking leave at the end of my contract to prepare myself for civilian life.');
-// }
-// console.log(response6);
-// document.getElementById('q6').innerHTML = response6;
 
-// var states = ['texas', 'illinois', 'california', 'washington'];
-// var response7 = prompt('Try guessing a state I\'ve been to.');
-// var answer7 = response7.toLowerCase();
-// var pass = false;
-// for (var j = 0; j < 5; j++) {
-//   console.log(`guess ${j}`);
-//   for (var k = 0; k < states.length; k++) {
-//     answer7 = response7.toLowerCase();
-//     console.log(answer7, states[k]);
-//     if (states[k] === answer7) {
-//       pass = true;
-//     }
-//   }
-//   if (pass === false) {
-//     response7 = prompt('Incorrect! Guess again.');
-//   }
-// }
-// if (pass) {
-//   alert('Correct! I\'ve been to Texas, Illinois, California and now Washington!');
-//   a++;
-// } else {
-//   alert('Incorrect! I\'ve been to Texas, Illinois, California and now Washington.');
-// }
-// console.log(response7);
-// document.getElementById('q7').innerHTML = response7;
+function question7() {
+  var states = ['texas', 'illinois', 'california', 'washington'];
+  var response7 = prompt('Try guessing a state I\'ve been to.');
+  var answer7 = response7.toLowerCase();
+  var pass2 = false;
+  for (var j = 0; j < 5; j++) {
+    console.log(`guess ${j}`);
+    for (var k = 0; k < states.length; k++) {
+      answer7 = response7.toLowerCase();
+      console.log(answer7, states[k]);
+      if (states[k] === answer7) {
+        pass2 = true;
+      }
+    }
+    if (pass2 === false) {
+      response7 = prompt('Incorrect! Guess again.');
+    }
+  }
+  if (pass2) {
+    alert('Correct! I\'ve been to Texas, Illinois, California and now Washington!');
+    a++;
+  } else {
+    alert('Incorrect! I\'ve been to Texas, Illinois, California and now Washington.');
+  }
+  console.log(response7);
+} question7();
 
 // console.log('You got ' + a + ' out of 7 correct.');
-alert('You got ' + a + ' out of 7 correct.');
+function finalScore() {
+  alert('You got ' + a + ' out of 7 correct.');
+}
+finalScore;
+
 // var result1 = ('You got ' + a + ' out of 7 correct.');
 // document.getElementById('result').innerHTML = result1;
